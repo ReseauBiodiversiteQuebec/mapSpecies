46312affffcf4498d3eae467129bbac1aebdbeb1
frousseu <francoisrousseu@hotmail.com>
2022-05-17 13:37:32 +0400

    mapSpace now gets all values and optionally samples from the posterior. Removed type argument and added a terra dependency.

--- R/mapSpace.R
+++ R/mapSpace.R
@@ -4,7 +4,6 @@
 #' 
 #' @param modelSpace An object of class \code{ppSpace} or of class \code{uniSpace}.
 #' @param dims A vector of length 2 defining the number of pixels to use as rows and columns to define the map.
-#' @param type Either "mean", "sd", "0.025quant", "0.5quant", "0.975quant", "mode" or "space". Defines the map to be drawn. If "space", maps the mean of the spatial field.
 #' @param sPoly A spatial polygon to isolate the region of interest. If none is given, a map is drawn for the entire region covered by the mesh. 
 #' @param sample Logical. Whether to sample from the posterior distribution of each cell using \code{INLA}'s \code{inla.posterior.sample}. Returns a raster stack of each sample along with other types specified. Default to \code{FALSE}. Currently ignored.
 #' @param nsamples Integer. Number of samples to draw from the posterior. Ignored if \code{sample = FALSE}.
@@ -19,18 +18,22 @@
 #' @importFrom raster ymin
 #' @importFrom raster xmax
 #' @importFrom raster ymax
+#' @importFrom terra rast
+#' @importFrom terra ext
 #'
 #' @keywords hplot
 #' 
 #' @export
 #' 
-mapSpace <- function(modelSpace, dims, 
-                       type = c("mean", "sd", "0.025quant", 
-                                "0.5quant", "0.975quant",
-                                "mode","space"), sPoly = NULL, sample = FALSE, nsamples = 100){
-  ### General check
-  if(length(type) > 1){
-    stop("Only one type should be defined")
+mapSpace <- function(modelSpace, dims, sPoly = NULL, sample = FALSE, nsamples = 100){
+  
+  ### Names to extract and/or assign to layers
+  valsPred<-c("mean", "sd", "0.025quant", "0.5quant", "0.975quant","mode")
+  valsSpat<-c("mean","sd")
+  if(sample){
+    valsSamp<-paste0("sample",formatC(1:nsamples,width=nchar(nsamples),flag=0))
+  }else{
+    valsSamp<-NULL
   }
   
   ### Define map basis
@@ -47,25 +50,48 @@ mapSpace <- function(modelSpace, dims,
   }
   
   
-  if(type=="space"){
+
+  ### Project spatial field values  
+  mapSpat <- as.matrix(inla.mesh.project(mapBasis, 
+                                           modelSpace$summary.random[['i']][,valsSpat]))
+
+  
+  ### Find the mesh edges on which predictions should be made
+  ID <- inla.stack.index(attributes(modelSpace)$Stack, tag="pred")$data
     
-    mapPred <- inla.mesh.project(mapBasis, 
-                                 modelSpace$summary.random[['i']][['mean']])
+  ### Project predicted values
+  mapPred <- as.matrix(inla.mesh.project(mapBasis, 
+                                           modelSpace$summary.fitted.values[ID,valsPred]))
+  
+  ### Sample from posterior and project sampled values  
+  if(sample){
+    class(modelSpace)<-"inla"
+    samps<-inla.posterior.sample(nsamples,modelSpace)
+    samps<-lapply(samps,function(i){
+      i$latent
+    })
+    samps<-do.call("cbind",samps)
+    vals<-samps[grep("i:",row.names(samps)),]
+    vals<-samps[1:nrow(vals),] # the second set from nrow(vals)+1 to 2*nrow(vals) is the same
+    mapSamp <- as.matrix(inla.mesh.project(mapBasis,vals))
+    mat<-cbind(mapPred,mapSpat,mapSamp)
   }else{
-  
-    ### Find the mesh edges on which predictions should be made
-    ID <- inla.stack.index(attributes(modelSpace)$Stack, tag="pred")$data
-  
-    ### Calculate prediction
-    mapPred <- inla.mesh.project(mapBasis, 
-                               modelSpace$summary.fitted.values[[type]][ID])
-  
+    mat<-cbind(mapPred,mapSpat)
   }
-  ### Transform map into a raster
-  mapRaster <- raster(t(mapPred[,ncol(mapPred):1]),
-                      xmn = min(mapBasis$x), xmx = max(mapBasis$x), 
-                      ymn = min(mapBasis$y), ymx = max(mapBasis$y))
-  
-  ### Return
-  return(mapRaster)
+    
+    
+  ### Rearrange and reorder values to put them in a raster stack
+  # Probably not the most efficient way to do that...
+  a<-array(rev(as.vector(mat)),dim=c(dims[1],dims[2],ncol(mat)))
+  a<-apply(a,3,t,simplify=FALSE)
+  a<-lapply(a,function(i){
+    i[,ncol(i):1]
+  })
+  a<-simplify2array(a)
+  mapRaster<-rast(a[,,dim(a)[3]:1]) # uses terra for now
+  ext(mapRaster)<-c(xmin = min(mapBasis$x), xmax = max(mapBasis$x),ymin = min(mapBasis$y), ymax = max(mapBasis$y))
+  mapRaster<-stack(mapRaster)
+  names(mapRaster)<-c(valsPred,paste0("space",valsSpat),valsSamp)
+
+  mapRaster
 }
