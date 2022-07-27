68224e795d4dfb348126084a391f3302320cc220
frousseu <francoisrousseu@hotmail.com>
2022-07-27 13:37:49 +0400

    now intersects before intersection and returns dmeshcuts for later

--- R/ppWeight.R
+++ R/ppWeight.R
@@ -18,23 +18,33 @@
 #' 
 #' @keywords models
 #' 
-ppWeight <- function(sPoly, mesh){
+ppWeight2 <- function(sPoly, mesh){
   #-------------------------------------------------
   ### Construct a dual mesh from the triangular mesh
   #-------------------------------------------------
   dmesh <- inla.mesh.dual(mesh)
-
+  
   #--------------------------------------------------------------
   ### Find the intersection between the polygons in the dual mesh
   ### and the location domain
   #--------------------------------------------------------------
-
+  
+  # An intersect and a within are used to first find polygons that will 
+  # be cut to reduce the duration of the intersection (about twice as fast)
+  
   ### Calculate weight
+  dmesh$id <- 1:nrow(dmesh)
   weight <- numeric(nrow(dmesh))
-  overlaps <- st_intersects(dmesh, st_as_sf(sPoly))
-  cuts <- st_intersection(dmesh, st_as_sf(sPoly))
-  w <- which(as.logical(sapply(overlaps, length)))
-  areas <- as.numeric(st_area(cuts))
+  overlaps <- lengths(st_intersects(dmesh, sPoly))
+  within <- lengths(st_within(dmesh, sPoly))
+  o <- overlaps > 0L & !within > 0L
+  suppressWarnings(
+    cuts <- st_intersection(sPoly, dmesh[o, ])
+  )
+  dmeshcuts <- rbind(cuts, dmesh[within > 0L, ])
+  dmeshcuts <- dmeshcuts[order(dmeshcuts$id), ]
+  w <- which(as.logical(overlaps))
+  areas <- as.numeric(st_area(dmeshcuts))
   if(length(w)!=length(areas)){
     stop("Number of resulting polygons different from the number of touching cells")
   }
@@ -46,7 +56,7 @@ ppWeight <- function(sPoly, mesh){
   }
   
   ### Return mesh
-  attributes(weight) <- list(mesh=mesh,dmesh=dmesh)
+  attributes(weight) <- list(mesh = mesh, dmesh = dmesh, dmeshcuts = dmeshcuts)
   
   class(weight) <- "ppWeight"
   
